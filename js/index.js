ec16d42de3cbbf21572e5e5d15c616b85d615dea
Axelvaisper <ca6p@ya.ru>
2017-07-09 20:20:47 +0300

    up

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#lightgallery').lightGallery({
+    pager: true,
+    loadYoutubeThumbnail: true,
+    youtubeThumbSize: 'default',
+    loadVimeoThumbnail: true,
+    vimeoThumbSize: 'thumbnail_medium'
+  });
+});
